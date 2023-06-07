ac9334f940ab4923a2a5734a68a9042d47634b0d
NAGARRO\souvikpaul <souvik.paul@nagarro.com>
2023-06-07 13:24:57 +0530

    build again

--- src/services/API_URL.ts
+++ src/services/API_URL.ts
@@ -1,2 +1,2 @@
-// export const API_URL = "http://3.6.126.43:5000/";
-export const API_URL = "http://localhost:5000/";
+export const API_URL = "http://3.110.135.64:5000/";
+//export const API_URL = "http://localhost:5000/";
